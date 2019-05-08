a2653382eeeba1f6ae571d5a48a181dd861a2a38
Filip Osowski <filiposowski5@gmail.com>
2019-05-08 23:29:00 +0000

    Add first iteration of test_case_creator.py

--- jshell.py
+++ jshell.py
@@ -1,14 +1,11 @@
 from time import sleep
 from base64 import b64encode
 import os
+import string
+import random
 import subprocess
 import atexit
 import signal
-
-def cleanup():
-    jshell.kill()
-
-atexit.register(cleanup)
 
 # Move load_snippet
 class JShell:
@@ -22,35 +19,64 @@ class JShell:
 
     def __init__(self):
         def random_string(n):
-            bytes_ = os.urandom(n)
-            string = b64encode(bytes_).decode('utf-8')
-            return string
+            return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+        def mkdir(path):
+            try:
+                os.makedirs(path)
+            except OSError as exc:
+                pass
+
+        mkdir("snippets")
+        mkdir("fifos")
 
         self.EOF_key = random_string(32)
         self.snippet_key = random_string(32)
         self.fifo_key = random_string(32)
-        self.snippet_path = "./snippets/" + self.snippet_key
-        self.in_path = "./fifos/" + self.fifo_key + "_in"
-        self.out_path = "./fifos/" + self.fifo_key + "_out"
+        snippet_path = "snippets/" + self.snippet_key
+        in_path = "fifos/" + self.fifo_key + "_in"
+        out_path = "fifos/" + self.fifo_key + "_out"
 
-        for path in [self.in_path, self.out_path]:
+        for path in [in_path, out_path]:
             try:
                 os.mkfifo(path)
             except FileExistsError:
                 pass
 
-        self.dummy_input_process = subprocess.Popen("(while true; do sleep 86400; done) > in", shell=True, preexec_fn=os.setsid)
-        self.jshell_process = subprocess.Popen("jshell < in > out", shell=True, cwd=".", preexec_fn=os.setsid)
+        dummy_input_process = subprocess.Popen(f"(while true; do sleep 86400; done) > {in_path}", shell=True, preexec_fn=os.setsid)
+        jshell_process = subprocess.Popen(f"jshell < {in_path} > {out_path}", shell=True, cwd=".", preexec_fn=os.setsid)
+
+        # This is needed so that exit() can refer to both
+        # processes without using self.{process}.
+        # This way, garbage collection of this jshell
+        # instances works properly.
+        self.dummy_input_process = dummy_input_process
+        self.jshell_process = jshell_process
+        self.in_path = in_path
+        self.out_path = out_path
+        self.snippet_path = snippet_path
 
         with open(self.out_path) as java_out:
             java_out.read(89)
 
-        self.run("/set feedback concise")
+        self.run("/set feedback concise", blocking=True)
 
-    
+        def exit():
+            os.killpg(os.getpgid(jshell_process.pid), signal.SIGTERM)
+            os.killpg(os.getpgid(dummy_input_process.pid), signal.SIGTERM)
+
+            for f in [in_path, out_path, snippet_path]:
+                try:
+                    os.remove(f)
+                except FileNotFoundError:
+                    pass
+
+        atexit.register(exit)
+
+
     def kill(self):
-        os.killpg(os.getpgid(jshell.jshell_process.pid), signal.SIGTERM)
-        os.killpg(os.getpgid(jshell.dummy_input_process.pid), signal.SIGTERM)
+        os.killpg(os.getpgid(self.jshell_process.pid), signal.SIGTERM)
+        os.killpg(os.getpgid(self.dummy_input_process.pid), signal.SIGTERM)
 
         for f in [self.in_path, self.out_path, self.snippet_path]:
             try:
@@ -59,9 +85,9 @@ class JShell:
                 pass
 
 
-    def write(self, string):
+    def write(self, string_):
         with open(self.in_path, 'w') as java_in:
-            java_in.write(string)
+            java_in.write(string_)
 
 
     def read(self, num_bytes):
@@ -77,7 +103,10 @@ class JShell:
             java_in.write("\n //" + self.EOF_key + "\n")
 
 
-    def run(self, code, q):
+    def run(self, code, q=None, blocking=False):
+        if not blocking and q == None:
+            raise Exception("If blocking=False, a queue must be passed in.")
+
         num_bytes = len(code.encode('utf-8'))
         self.write(code)
         self.read(num_bytes)
@@ -86,11 +115,12 @@ class JShell:
         output = []
         with open(self.out_path) as java_out:
             for i, line in enumerate(java_out):
-                # print("LINE", repr(line))
                 if self.EOF_key in line:
                     break
                 elif i:
                     output.append(line)
 
-
-        q.put(output)
+        if not blocking:
+            q.put(output)
+        else:
+            return output
--- test_case_creator.py
+++ test_case_creator.py
@@ -1,1 +1,69 @@
-# Nothing yet
+from jshell import JShell
+import sys
+import importlib.util
+import json
+import jinja2
+
+problem_URL = "/home/filip/image/2004/1/a/"
+num_test_cases = 100
+arg_generator = []
+jshell = JShell()
+setup_ended_key = "//``SETUP ENDED``" 
+
+def load_template(template_URL):
+    templateLoader = jinja2.FileSystemLoader(searchpath="/")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template(template_URL)
+    return template
+
+
+def get_arg_generators(arg_generator_directory_URL):
+    from inspect import getmembers, isfunction
+
+    # Need to add new path to locate & import arg_generators
+    sys.path.append(arg_generator_directory_URL)
+    import arg_generators
+
+    # functions are sorted by alphabetical order, meaning
+    # functions _2, _3, _1 will be automatically sorted to
+    # _1, _2, _3.
+    functions_list = [o[1] for o in getmembers(arg_generators) if isfunction(o[1])]
+
+    return functions_list
+
+
+def create_test_cases(problem_URL):
+    test_cases = []
+    solution = None
+    with open(problem_URL + "solution.java") as s:
+        solution = s.read()
+
+    jshell.run(solution, blocking=True)
+
+    test_case_template = load_template(problem_URL + "test_case_template.java")
+    test_case_args = []
+    arg_generators = get_arg_generators(problem_URL)
+
+    for _ in range(num_test_cases):
+        template_args = {}
+        for i, arg_generator in enumerate(arg_generators):
+            template_args[str(i)] = arg_generator()
+
+        input_ = test_case_template.render(template_args)
+
+        output = []
+        setup_ended = False
+
+        for line in input_:
+            if setup_ended_key in line:
+                setup_ended = True
+
+            if setup_ended:
+                output.append(o)
+
+        test_cases.append({"input": input_, "output": output})
+
+    with open(problem_URL + "test_cases.json", "w") as test_case_file:
+        json.dump(test_cases, test_case_file)
+
+create_test_cases(problem_URL)
